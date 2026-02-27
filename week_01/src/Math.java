863645f4107d093ebd940c6467bdc1a339be8d8b
Adam Bastian Chaniago <154961246+Coastaxx@users.noreply.github.com>
2026-02-27 13:25:21 +0700

    upload week_01 calculactor

---
+++ week_01/src/Math.java
@@ -0,0 +1,18 @@
+public class Math { // membuat class Math
+    double no1; // notegwh: proprty (variable untuk menampung data)
+    double no2;
+    double no3;
+
+    // notegwh: ini adalah method "add, multiply, devide"
+    public double add() {
+        return no1 + no2 + no3;
+    }
+
+    public double multiply() {
+        return no1 * no2 * no3;
+    }
+
+    public double devide() {
+        return (no1 + no2) /  no3;
+    }
+}
--- week_01/src/app.java
+++ week_01/src/app.java
@@ -1,7 +1,17 @@
-public class App {
-    public static void main(String[] args) {
-        User x = new User("1122334455", "Adam Bastian");
+public class App{
+    public static void main(String[] args) throws Exception {
+        // notegwh: Membuat objek mycalculactor (memanggil mesin Math ke dalam App)
+        Math mycalculactor = new Math();
+
+        mycalculactor.no1 = 10;
+        mycalculactor.no2 = 2;
+        mycalculactor.no3 = 3;
+
+        System.out.println("== Hasil perhitungan ==");
         
-        x.introduce();
+        // memanggil method add, multiply dan devide
+        System.out.println("Hasil dari 10 + 2 + 3 =" + mycalculactor.add());
+        System.out.println("Hasil dari 10 * 2 * 3 =" + mycalculactor.multiply());
+        System.out.println("Hasil dari (10 + 2) * 3 =" + mycalculactor.devide());
     }
 }
